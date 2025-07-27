c612e6293caec34d9fad85e7021293f4dfb9f84e
raziel9856 <pedro.parrales@hotmail.com>
2025-07-27 16:36:57 -0500

    feat(): implement custom token view and user info endpoint

--- users/api/router.py
+++ users/api/router.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from users.api.views import UserApiViewSet, users_are_activate, users_waiting_for_activate, roles_all
+from users.api.views import UserApiViewSet, users_are_activate, users_waiting_for_activate, roles_all, CustomTokenObtainPairView, get_info_user
 
 router_user = DefaultRouter()
 router_user.register(prefix='', basename='users', viewset=UserApiViewSet)
 
 urlpatterns = [
-    path('auth/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/login', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/me', get_info_user),
     path('user/are-activate-by-university/<id_university>', users_are_activate),
     path('user/waiting-for-activate-by-university/<id_university>', users_waiting_for_activate),
     path('role/', roles_all),
